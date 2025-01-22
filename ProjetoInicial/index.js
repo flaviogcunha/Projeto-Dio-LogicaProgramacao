e11b537cfc88eb52a0eb2867150ff6227eb6c1eb
flaviogcunha <144805724+flaviogcunha@users.noreply.github.com>
2025-01-22 18:51:36 -0300

    Primeiro Projeto
    Dio logica de Programação

---
+++ ProjetoInicial/index.js
@@ -0,0 +1,36 @@
+let nomeHeroi = "fafa Gamers"
+let xpdoHeroi = 9567
+let nivel = ""
+
+if (xpdoHeroi <= 1000){
+    nivel = "Ferro"
+    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivel)
+}
+else if (xpdoHeroi > 1000 && xpdoHeroi<= 2000){
+    nivel = "Bronze"
+    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivel)
+}
+else if (xpdoHeroi > 2000 && xpdoHeroi<= 5000){
+    nivel = "Prata"
+    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivel)
+}
+else if (xpdoHeroi > 5000 && xpdoHeroi<= 7000){
+    nivel = "Ouro"
+    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivel)
+}
+else if (xpdoHeroi > 7000 && xpdoHeroi<= 8000){
+    nivel = "Platina"
+    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivel)
+}
+else if (xpdoHeroi > 8000 && xpdoHeroi<= 9000){
+    nivel = "Ascendente"
+    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivel)
+}
+else if (xpdoHeroi > 9000 && xpdoHeroi<= 10000){
+    nivel = "Imortal"
+    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivel)
+}
+else if (xpdoHeroi > 10000){
+    nivel = "Radiante"
+    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivel)
+}
